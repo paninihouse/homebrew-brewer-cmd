d8521a0fc73006d29a6d480bda11eea0c494b8ec
Tommaso Negri <tommaso@hey.com>
2024-10-03 10:16:08 +0200

    Migrate to AbstractCommand

--- cmd/brewer-tap-content-info.rb
+++ cmd/brewer-tap-content-info.rb
@@ -4,54 +4,52 @@ require "formula"
 require "json"
 
 module Homebrew
-  module_function
-
-  def brewer_tap_content_info_args
-    Homebrew::CLI::Parser.new do
-      description <<~EOS
-        Do something. Place a description here.
-      EOS
-      flag "--json",
-           description: "Print a JSON representation. Currently the default value for <version> is `v1` for " \
-                        "<formula>. For <formula> and <cask> use `v2`. See the docs for examples of using the " \
-                        "JSON output: <https://docs.brew.sh/Querying-Brew>"
-      switch "--formula", "--formulae",
-             description: "Treat all named arguments as formulae."
-      switch "--cask", "--casks",
-             description: "Treat all named arguments as casks."
-      conflicts "--formula", "--cask"
-      
-      named_args [:tap]
-    end
-  end
-
-  def brewer_tap_content_info
-    args = brewer_tap_content_info_args.parse
-    
-    formulae = []
-    casks = []
-    
-    Tap.each do |tap|
-      next if ["homebrew/core", "homebrew/cask"].include?(tap.name)
-      
-      unless args.cask?
-        tap.formula_files.each do |formula_file|
-          formulae << Formulary.factory(formula_file)
-        end
+  module Cmd
+    class BrewerTapContentInfo < AbstractCommand
+      cmd_args do
+        description <<~EOS
+          Do something. Place a description here.
+        EOS
+        flag "--json",
+             description: "Print a JSON representation. Currently the default value for <version> is `v1` for " \
+                          "<formula>. For <formula> and <cask> use `v2`. See the docs for examples of using the " \
+                          "JSON output: <https://docs.brew.sh/Querying-Brew>"
+        switch "--formula", "--formulae",
+               description: "Treat all named arguments as formulae."
+        switch "--cask", "--casks",
+               description: "Treat all named arguments as casks."
+        conflicts "--formula", "--cask"
+        
+        named_args [:tap]
       end
       
-      unless args.formula?
-        tap.cask_files.each do |cask_file|
-          casks << Cask::CaskLoader::FromPathLoader.new(cask_file).load(config: nil)
+      def run
+        formulae = []
+        casks = []
+        
+        Tap.each do |tap|
+          next if ["homebrew/core", "homebrew/cask"].include?(tap.name)
+          
+          unless args.cask?
+            tap.formula_files.each do |formula_file|
+              formulae << Formulary.factory(formula_file)
+            end
+          end
+          
+          unless args.formula?
+            tap.cask_files.each do |cask_file|
+              casks << Cask::CaskLoader::FromPathLoader.new(cask_file).load(config: nil)
+            end
+          end
         end
+        
+        json = {
+          "formulae": formulae.map(&:to_hash),
+          "casks": casks.map(&:to_h)
+        }
+        
+        puts JSON.pretty_generate(json)
       end
     end
-    
-    json = {
-      "formulae": formulae.map(&:to_hash),
-      "casks": casks.map(&:to_h)
-    }
-    
-    puts JSON.pretty_generate(json)
   end
 end
