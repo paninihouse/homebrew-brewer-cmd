e9f09adde293de89c9eb9f9d8f84e4265a604173
Tommaso Negri <tommaso@hey.com>
2024-12-03 14:39:24 +0100

    Prevent warning and errors from breaking the sync

--- cmd/brewer-tap-content-info.rb
+++ cmd/brewer-tap-content-info.rb
@@ -2,13 +2,14 @@
 
 require "formula"
 require "json"
+require "readall"
 
 module Homebrew
   module Cmd
     class BrewerTapContentInfo < AbstractCommand
       cmd_args do
         description <<~EOS
-          Do something. Place a description here.
+          Display the information about third-party casks and formulas.
         EOS
         flag "--json",
              description: "Print a JSON representation. Currently the default value for <version> is `v1` for " \
@@ -19,36 +20,52 @@ module Homebrew
         switch "--cask", "--casks",
                description: "Treat all named arguments as casks."
         conflicts "--formula", "--cask"
-        
-        named_args [:tap]
       end
       
       def run
-        formulae = []
-        casks = []
-        
-        Tap.each do |tap|
-          next if ["homebrew/core", "homebrew/cask"].include?(tap.name)
+        begin
+          original_stderr = $stderr.clone
+          $stderr.reopen(File.new("/dev/null", "w"))
           
-          unless args.cask?
-            tap.formula_files.each do |formula_file|
-              formulae << Formulary.factory(formula_file)
+          formulae = []
+          casks = []
+          
+          Tap.each do |tap|
+            next if ["homebrew/core", "homebrew/cask"].include?(tap.name)
+            next unless valid_tap? tap
+            
+            unless args.cask?
+              tap.formula_files.each do |formula_file|
+                formulae << Formulary.factory(formula_file)
+              end
+            end
+            
+            unless args.formula?
+              tap.cask_files.each do |cask_file|
+                casks << Cask::CaskLoader::FromPathLoader.new(cask_file).load(config: nil)
+              end
             end
           end
           
-          unless args.formula?
-            tap.cask_files.each do |cask_file|
-              casks << Cask::CaskLoader::FromPathLoader.new(cask_file).load(config: nil)
-            end
-          end
+          json = {
+            "formulae": formulae.map(&:to_hash),
+            "casks": casks.map(&:to_h)
+          }
+          
+          puts JSON.pretty_generate(json)
+        ensure
+          $stderr.reopen(original_stderr)
         end
-        
-        json = {
-          "formulae": formulae.map(&:to_hash),
-          "casks": casks.map(&:to_h)
-        }
-        
-        puts JSON.pretty_generate(json)
+      end
+      
+      private
+      
+      def valid_tap?(tap)
+        begin
+          Readall.valid_tap?(tap, aliases: true)
+        rescue Interrupt, RuntimeError
+          false
+        end
       end
     end
   end
